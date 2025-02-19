7f88c9e918b07290d4d1acffbade82dba74349aa
totomanov <totomanov@protonmail.com>
2025-02-19 22:58:48 +0200

    fix: export indexRegistry

--- src/euler/index.ts
+++ src/euler/index.ts
@@ -2,5 +2,6 @@ export * from './fetchDeployedRouters';
 export * from './indexAdapters';
 export * from './indexAssets';
 export * from './indexRouterHistoricalAdapters';
+export * from './indexRegistry';
 export * from './types';
 export * from './artifacts';
