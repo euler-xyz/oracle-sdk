92141ea0d5a276aae8a10775e6a1fd37d489bafd
Georgi Angelchev <gogo.angelchev006@gmail.com>
2025-09-18 23:01:54 +0300

    hotfix: redstone base url

--- src/redstone/constants.ts
+++ src/redstone/constants.ts
@@ -1,2 +1,2 @@
 export const REDSTONE_MANIFESTS_BASE_URL =
-  'https://raw.githubusercontent.com/redstone-finance/redstone-oracles-monorepo/refs/heads/main/packages/on-chain-relayer/';
+  'https://raw.githubusercontent.com/redstone-finance/redstone-oracles-monorepo/refs/heads/main/packages/relayer-remote-config/main/';
