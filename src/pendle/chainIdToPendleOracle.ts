bf7788a5f465d8fd90131fc6a143baa1f3304c10
kanv <kanv@euler.xyz>
2025-10-22 13:58:41 +0200

    feat: add plasma pendle oracle

--- src/pendle/chainIdToPendleOracle.ts
+++ src/pendle/chainIdToPendleOracle.ts
@@ -1,5 +1,5 @@
 import { Address } from 'viem';
-import { arbitrum, base, bsc, mainnet, mantle, optimism } from 'viem/chains';
+import { arbitrum, base, bsc, mainnet, mantle, optimism, plasma } from 'viem/chains';
 
 export const chainIdToPendleOracle: Record<number, Address> = {
   [mainnet.id]: '0x9a9Fa8338dd5E5B2188006f1Cd2Ef26d921650C2',
@@ -8,4 +8,5 @@ export const chainIdToPendleOracle: Record<number, Address> = {
   [mantle.id]: '0x9a9Fa8338dd5E5B2188006f1Cd2Ef26d921650C2',
   [bsc.id]: '0x9a9Fa8338dd5E5B2188006f1Cd2Ef26d921650C2',
   [base.id]: '0x9a9Fa8338dd5E5B2188006f1Cd2Ef26d921650C2',
+  [plasma.id]: '0x9a9Fa8338dd5E5B2188006f1Cd2Ef26d921650C2',
 };
